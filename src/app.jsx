9964329cbcced18420c27c34978ca7002a0be488
ezn6 <kcourtney@naver.com>
2022-08-22 16:07:14 +0900

    no message

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './app.module.css';
 import Login from './components/login/login';
-import { useState } from 'react';
 import styles from './app.module.css';
 import Maker from './components/maker/maker';
-import Preview from './components/preview/preview';
 
 function App({ auth }) {
-  const [login, setLogin] = useState(false);
-
   return (
     <div className={styles.app}>
       <Routes>
-        <Route
-          path='/'
-          element={<Login auth={auth} login={login} setLogin={setLogin} />}
-        />
-        <Route
-          path='/maker'
-          element={<Maker auth={auth} login={login} setLogin={setLogin} />}
-        />
-        <Route path='/preview' element={<Preview />} />
+        <Route path='/' element={<Login auth={auth} />} />
+        <Route path='/maker' element={<Maker auth={auth} />} />
         <Route
           path='*'
           element={<h1 style={{ color: 'orange' }}>PAGE NOT FOUND😥</h1>}
